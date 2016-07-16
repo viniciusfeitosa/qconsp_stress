bb08724d29cebcea331df6d41eb1ecd59e01281b
viniciusfeitosa <vfpweb@gmail.com>
2016-07-16 12:57:21 -0300

    first commit

---
+++ go_fib.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	mux := httprouter.New()
+	mux.GET("/:number", indexHandle)
+
+	log.Println("App started on port 4567")
+	log.Fatal(http.ListenAndServe(":4567", mux))
+}
+
+func indexHandle(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	n, _ := strconv.Atoi(ps.ByName("number"))
+	fmt.Fprintf(w, "Go + httprouter fib(%s): %d", ps.ByName("number"), fib(n))
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return n
+	}
+	if n == 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
